cfe314b3f6fb2db920e37ceffc31dd73553852df
Suraj1199 <suraj19995.ss@gmail.com>
2022-03-14 22:06:49 +0530

    Create File

---
+++ 1374B_Multiply_by_2_Divide_by_6.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    n = int(input())
+    cnt2, cnt3 = 0, 0
+    while n % 2 == 0:
+        n //= 2
+        cnt2 += 1 
+    while n % 3 == 0:
+        n //= 3
+        cnt3 += 1
+    if n == 1 and cnt2 <= cnt3:
+        print(2 * cnt3 - cnt2)
+    else:
+        print(-1)
