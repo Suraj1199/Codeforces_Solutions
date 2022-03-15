a4b4a93412ca74e725f8a4c00c1d33adc86dc2f0
Suraj1199 <suraj19995.ss@gmail.com>
2022-03-15 19:43:14 +0530

    Create File

---
+++ 1634B_Fortune_Telling.py
@@ -0,0 +1,8 @@
+t = int(input())
+for _ in range(t):
+    n, x, y = map(int, input().split())
+    a = list(map(int, input().split()))
+    if (sum(a) + x + y) % 2 == 0:
+        print('Alice')
+    else:
+        print('Bob')
