bb3c406ab84e7c11b3c0cb7edd5218a240a5b98a
Suraj1199 <suraj19995.ss@gmail.com>
2022-03-17 09:14:15 +0530

    Create File

--- 1618A_Polycarp_and_Sums_of_Subsequences.py
+++ 1618A_Polycarp_and_Sums_of_Subsequences.py
@@ -1,6 +1,3 @@
 for _ in range(int(input())):
-    b = input().split(" ")
-    if b[0] + b[1] != b[2]:
-        print(' '.join(b[:3]))
-    else:
-        print(' '.join(b[:2] + [b[4]]))
+    b = list(map(int, input().split(" ")))
+    print(b[0], b[1], b[-1] - b[0] - b[1])
