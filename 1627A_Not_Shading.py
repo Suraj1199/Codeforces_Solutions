17f37a005e2f45ff093737679c689a1210ed89d1
Suraj1199 <suraj19995.ss@gmail.com>
2022-05-03 21:24:06 +0530

    Create File

---
+++ 1627A_Not_Shading.py
@@ -0,0 +1,7 @@
+for t in range(int(input())):
+    n, m, r, c = map(int, input().split())
+    a = [list(input()) for i in range(n)]
+    if "B" not in str(a):
+        print(-1)
+        continue
+    print(2 - ("B" in a[r-1] + list([*zip(*a)][c-1])) - (a[r-1][c-1] == 'B'))
