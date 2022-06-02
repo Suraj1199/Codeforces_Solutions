8b6248dd104804198a4a3799cbed868b7efb3205
Suraj1199 <suraj19995.ss@gmail.com>
2022-06-02 10:44:04 +0530

    Create File

--- 1183A_Nearest_Interesting_Number.py
+++ 1183A_Nearest_Interesting_Number.py
@@ -1,11 +1,10 @@
-def sum(a):
-    
-    return result
-a = int(input())
-result = 0
-while a > 0:
-    result += a % 10
-    a //= 10
-while result % 4 != 0:
-    a += 1
-print(a)
+x = int(input())
+while True:
+    s, n = 0, x
+    while n > 0:
+        s += n % 10
+        n //= 10
+    if s % 4 == 0:
+        break
+    x += 1
+print(x)
